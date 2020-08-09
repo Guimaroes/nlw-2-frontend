94276e16604c18f2f779b2ab629723c819ed9f27
Guimaroes <gabriel.guimaraes@universo.univates.br>
2020-08-09 19:53:13 -0300

    NLW2/EP3 Frontend

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,35 +2,56 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+    
     return(
         <article className="teacher-item">
             <header>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZ7wUJmOTg7Nt7fNgnJ15a2OFvJ9iQFCFZ5A&usqp=CAU" alt="Walter White"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Walter White</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Entusiasta das melhores tecnologias de química avançada.
-                <br /><br />
-                Apaixonado por explodir coisas em laboratórios e por mudar a vida das pessoas através de experiências.
-                Mais de 200.000 mil pessoas já passaram por uma das minhas explosões.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a 
+                    target="_blank"
+                    onClick={createNewConnection}
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
